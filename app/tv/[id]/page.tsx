0ec4bcf535a1dd2568fe21f1be24a981a8730663
Donald2023-source <donalddyusuf@gmail.com>
2025-11-29 17:04:54 +0100

    style: alignments

--- app/tv/[id]/page.tsx
+++ app/tv/[id]/page.tsx
@@ -122,7 +122,7 @@ const Page = () => {
       <div>
         
         <div className="flex md:gap-12  md:flex-row flex-col py-7 items-start">
-          <div className="md:pl-5 md:px-10 px-2 md:w-1/2">
+          <div className="md:pl-4 md:px-10 px-2 md:w-1/2">
             <h2 className="ml-4 md:ml-5 my-4 font-bold text-xl border-b-[1px] w-fit py-2">
               {details?.title || details?.original_name}
             </h2>
